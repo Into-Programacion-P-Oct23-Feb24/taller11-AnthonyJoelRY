fe68ed71770b4a8e79fff9237e84ffdea6703fda
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2024-01-04 17:35:03 -0500

    C1

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.*;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,42 @@ public class Problema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int x;
+        System.out.print("Ingrese el proceso que desea realizar:");
+        x = entrada.nextInt();
+        if (x == 1) {
+            obtenerAreaCuadrado();
+        } else {
+            if (x == 2) {
+                obtenerAreaTriangulo();
+            } else {
+                if (x == 3) {
+                    obtenerAreaRectangulo();
+                } else {
+                    System.out.println("Dato no valido");
+                }
+
+            }
+        }
+
     }
-    
+
+    public static void obtenerAreaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        double l1;
+        System.out.println("Escribir L1");
+        l1 = entrada.nextDouble();
+        l1 = l1 * 4;
+        System.out.print("El area del cuadrado es:"+l1);
+    }
+
+    public static void obtenerAreaTriangulo() {
+
+    }
+
+    public static void obtenerAreaRectangulo() {
+
+    }
+
 }
