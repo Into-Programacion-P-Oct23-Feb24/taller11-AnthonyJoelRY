cc5c198956bc3218d36b7865f97f29ec263a93e9
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2024-01-05 12:58:51 -0500

    CF

--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,10 +5,43 @@
  */
 package problemas;
 
+import java.util.*;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    static int numeroDatos;
+
+    public static void main(String[] args) {
+        String[] informacion = new String[numeroDatos];
+        informacion = obtenerDatos();
+        obtenerDatos2(informacion);
+    }
+
+    public static String[] obtenerDatos() {
+        Scanner entrada = new Scanner(System.in);
+        String datos = "";
+        System.out.print("Ingrese el numero de datos a ingresar:");
+        numeroDatos = entrada.nextInt();
+        String[] informacion = new String[numeroDatos];
+        entrada.nextLine();
+        for (int i = 0; i < informacion.length; i++) {
+            System.out.printf("Ingrese el dato#%d:", i + 1);
+            datos = entrada.nextLine();
+            informacion[i] = datos;
+        }
+        return informacion;
+    }
+
+    public static void obtenerDatos2(String[] informacion) {
+
+        for (int i = 0; i < informacion.length; i++) {
+            if (informacion[i].length() == 4 || informacion[i].length() == 5) {
+                System.out.println(informacion[i]);
+            }
+        }
+    }
 }
