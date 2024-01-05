9a93757231ab2391ee9097d52e72b6840f646e62
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2024-01-05 10:08:58 -0500

    C2

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.*;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,41 @@ public class Problema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        String resultado;
+        double n1;
+        double n2;
+        double n3;
+        double n4;
+
+        System.out.print("Ingrese nota#1 :");
+        n1 = entrada.nextDouble();
+        System.out.print("Ingrese nota#2 :");
+        n2 = entrada.nextDouble();
+        System.out.print("Ingrese nota#3 :");
+        n3 = entrada.nextDouble();
+        System.out.print("Ingrese nota#4 :");
+        n4 = entrada.nextDouble();
+
+        resultado = obtenerParametros(n1, n2, n3, n4);
+        System.out.println("El promedio de las notas: "
+                + n1 + ", " + n2 + ", " + n3 + ", " + n4 + " es " + resultado);
     }
-    
+
+    public static String obtenerParametros(double n1, double n2, double n3, double n4) {
+        double promedio = (n1 + n2 + n3 + n4) / 4;
+
+        if (promedio >= 0 && promedio <= 5) {
+            return "Regular";
+        } else if (promedio <= 8) {
+            return "Bueno";
+        } else if (promedio <= 9) {
+            return "Muy Bueno";
+        } else if (promedio <= 10) {
+            return "Sobresaliente";
+        } else {
+            return "Promedio fuera de rango";
+        }
+    }
+
 }
